37cb541ccb76e1089cf6d9d24de0a5feffa05b1c
andersonjmv <anderson.manosalva@gmail.com>
2021-12-03 13:50:58 -0500

    Api basica

---
+++ src/routes/user.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const userSchema = require("../models/user");
+
+const router = express.Router();
+
+// Crear Usuario
+router.post("/users", (req, res) => {
+  const user = userSchema(req.body);
+  user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Traer todos los usuarios
+router.get("/users", (req, res) => {
+  userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Traer un usuario(id)
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Eliminar usuario
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Actualizar usuario
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, age, email } = req.body;
+  userSchema
+    .updateOne({ _id: id }, { $set: { name, age, email } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
